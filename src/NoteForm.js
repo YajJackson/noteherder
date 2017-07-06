81d7fd29b5fbbfdbc2802d023c5133506af81a04
Jay Nicholas Jackson <jaynickjackson@gmail.com>
2017-07-06 15:01:37 -0400

    add NoteForm component contents

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -4,7 +4,25 @@ import './NoteForm.css'
 
 class NoteForm extends Component {
     render() {
-        return <p>Note Form</p>
+        return (
+            <div className="NoteForm">
+                <div className="form-actions">
+                    <button type="button">
+                        <i className="fa fa-trash-o"></i>
+                    </button>
+                </div>
+                <form>
+                    <p>
+                        <input
+                            type="text"
+                            name="title"
+                            placeholder="Title your note"
+                        />
+                    </p>
+                <textarea name="body"></textarea>
+                </form>
+            </div>
+        )
     }
 }
 
