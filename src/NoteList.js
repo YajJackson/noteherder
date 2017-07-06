813755e80c4497d760738b64d7a1c34645f57d95
Jay Nicholas Jackson <jaynickjackson@gmail.com>
2017-07-06 15:56:57 -0400

    render notes from an object

--- src/NoteList.js
+++ src/NoteList.js
@@ -4,24 +4,26 @@ import './NoteList.css'
 import Note from './Note'
 
 const NoteList = () => {
-  const notes = [
-    {
+  const notes = {
+    'note-1': {
       id: 'note-1',
       title: 'Note Title',
       body: 'Some exmaple text here.',
     },
-    {
+    'note-2': {
       id: 'note-2',
       title: 'Note Title 2',
       body: 'Exmaple text two.'
     },
-  ]
+  }
+
+  const noteIds = Object.keys(notes)
 
   return (
     <div className="NoteList">
       <h3>Notes</h3>
       <ul id="notes">
-        {notes.map(note => <Note note={note} />)}
+        {noteIds.map(noteId => <Note note={notes[noteId]} />)}
       </ul>
     </div>
   )
