175e37fad61f8dc634552cda5066bf5fa62220be
Jay Nicholas Jackson <jaynickjackson@gmail.com>
2017-07-06 14:23:22 -0400

    add Sidebar, NoteList, NoteForm components

---
+++ src/NoteForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+import './NoteForm.css'
+
+class NoteForm extends Component {
+    render() {
+        return <p>Note Form</p>
+    }
+}
+
+export default NoteForm
---
+++ src/Sidebar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './Sidebar.css'
+
+const Sidebar = () => {
+    return <p>Sidebar</p>
+}
+
+export default Sidebar
