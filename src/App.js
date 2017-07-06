897477fb4697bcd83a634e15bdf56024e8e51377
Jay Nicholas Jackson <jaynickjackson@gmail.com>
2017-07-06 13:46:04 -0400

    remove some boilerplate things

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import base from './base'
 
-import './App.css';
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        Noteherder
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
