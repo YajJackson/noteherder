3be011605dc0f46488a8fd19b5e197dd7c4086a0
Jay Nicholas Jackson <jaynickjackson@gmail.com>
2017-07-06 16:07:52 -0400

    store Notes in App.js for easier access

--- src/App.js
+++ src/App.js
@@ -5,10 +5,29 @@ import './App.css'
 import Main from './Main'
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      notes: {
+        'note-1': {
+          id: 'note-1',
+          title: 'Note Title',
+          body: 'Some exmaple text here.',
+        },
+        'note-2': {
+          id: 'note-2',
+          title: 'Note Title 2',
+          body: 'Exmaple text two.'
+        },
+      }
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <Main />
+        <Main notes={this.state.notes} />
       </div>
     )
   }
