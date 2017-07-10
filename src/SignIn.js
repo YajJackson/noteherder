05c2ddab8ff332fee2abe82089d0bb3ca95b9d0d
Jay Nicholas Jackson <jaynickjackson@gmail.com>
2017-07-10 11:21:04 -0400

    style sign in page

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,23 +1,35 @@
 import React from 'react'
 
 import './SignIn.css'
+import quill from './quill.svg'
+import googleLogo from './google.svg'
 import { auth, googleProvider } from './base'
 
 const SignIn = () => {
-    const authenticate = () => {
-      auth
-        .signInWithPopup(googleProvider)
-    }
+  const authenticate = () => {
+    auth.signInWithPopup(googleProvider)
+  }
 
-    return (
-      <div className='SignIn'>
+  return (
+    <div className="SignIn">
+      <header className="Header">
+        <img src={quill} alt="" />
+        <span className="title">Noteherder</span>
+      </header>
+      <main>
+        <h3>Hey, Nerd! You Like Notes?</h3>
+        <p>You never know when you'll need to write crap down. In fact, you should probably be taking notes right now.</p>
+
         <button
+          className="google"
           onClick={authenticate}
         >
-          Sign In
+          <img src={googleLogo} alt="" />
+          Sign in with Google
         </button>
-      </div>
-    )
+      </main>
+    </div>
+  )
 }
 
 export default SignIn
