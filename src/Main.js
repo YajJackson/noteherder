eecb5194c541124145bf5f2d463ed1ee7cc9ee94
Jay Nicholas Jackson <jaynickjackson@gmail.com>
2017-07-12 09:56:33 -0400

    redirect based on signed in status

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 import './Main.css'
 import Sidebar from './Sidebar'
@@ -6,21 +7,38 @@ import NoteList from './NoteList'
 import NoteForm from './NoteForm'
 
 const Main = (props) => {
+    const formProps = {
+      notes: props.notes,
+      currentNote: props.currentNote,
+      saveNote: props.saveNote,
+      removeCurrentNote: props.removeCurrentNote,
+    }
     return (
         <div className='Main'>
             <Sidebar
               resetCurrentNote={props.resetCurrentNote}
               signOut={props.signOut}
             />
-            <NoteList 
+            <NoteList
               notes={props.notes}
               setCurrentNote ={props.setCurrentNote}
             />
-            <NoteForm 
-              currentNote={props.currentNote}
-              saveNote={props.saveNote}
-              removeCurrentNote={props.removeCurrentNote}
-            />
+
+            <Switch>
+              <Route path='/notes/:id' render={navProps => (
+                <NoteForm 
+                  {...formProps}
+                  {...navProps}
+                />
+              )} />
+
+              <Route render={navProps => (
+                <NoteForm 
+                  {...formProps}
+                  {...navProps}
+                />
+              )} />
+            </Switch>
         </div>
     )
 }
