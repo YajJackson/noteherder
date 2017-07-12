c2dd44479a6123f1b1ce7a34cc6527a52caead8c
Jay Nicholas Jackson <jaynickjackson@gmail.com>
2017-07-12 09:29:49 -0400

    Add routes for sign in and main

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import base, {auth} from './base'
+import {Route, Switch, Redirect} from 'react-router-dom'
 
 import './App.css'
 import Main from './Main'
@@ -138,7 +139,11 @@ class App extends Component {
   render() {    
     return (
       <div className="App">
-        { this.signedIn() ? this.renderMain(): <SignIn /> }
+        <Switch>
+          <Route path='/sign-in' component={SignIn} />
+          <Route path='/notes' render={() => this.renderMain()} />
+          <Route render={() => <Redirect to='/notes' />} />
+        </Switch>
       </div>
     )
   }
